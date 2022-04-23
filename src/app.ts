eaa8d9d7b73d21b1c54887346c7ddb9db5a4c098
Rifat-Hossain-Khan <rifat.hk889@gmail.com>
2022-04-24 02:11:42 +0600

    basic

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-import "dotenv/config";
 import express from "express";
 import config from "config";
 import logger from "./utils/logger";
+import connect from "./utils/connect";
 
 const port = config.get<number>("port");
 
@@ -11,4 +11,5 @@ app.use(express.json());
 
 app.listen(port, async () => {
   logger.info(`App is running at http://localhost:${port}`);
+  await connect();
 });

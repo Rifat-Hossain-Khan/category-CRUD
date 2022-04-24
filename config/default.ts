9b87cb74ff834d974331a25df7c75490be05b7db
Rifat-Hossain-Khan <rifat.hk889@gmail.com>
2022-04-25 04:15:54 +0600

    redis added

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,6 @@
 export default {
   port: 8000,
   dbUri: "mongodb://localhost:27017/category",
+  redis_port: 6379,
+  redis_EXP: 3600
 };
--- src/controller/category.controller.ts
+++ src/controller/category.controller.ts
@@ -2,11 +2,19 @@ import { DeleteCategoryInput, ReadCategoryInput } from "./../schema/category.sch
 import { createCategory, findCategory, findCategories, findAndUpdateCategory, updateCategory, deleteCategory } from "./../service/category.service";
 import { Request, Response } from "express";
 import logger from "../utils/logger";
+import { createClient } from "redis";
+import config from "config";
+
+const redisClient = createClient();
+redisClient.connect();
+
+const DEFAULT_EXPIRATION = config.get<number>("redis_EXP");
 
 export const createCategoryHandler = async (req: Request, res: Response) => {
   try {
     logger.info(req.body);
     const category = await createCategory(req.body);
+    await redisClient.del(["activeCategories", "deActiveCategories"]);
     return res.send({ message: "Category created successfully!", data: category });
   } catch (e: any) {
     logger.error(e);
@@ -26,6 +34,8 @@ export const updateCategoryHandler = async (req: Request, res: Response) => {
 
     const updatedCategory = await findAndUpdateCategory({ _id: categoryId }, update, { new: true });
 
+    await redisClient.del([`category_${categoryId}`, "activeCategories", "deActiveCategories"]);
+
     return res.send({ message: "Category updated successfully!", data: updatedCategory });
   } catch (e: any) {
     logger.error(e);
@@ -36,10 +46,17 @@ export const updateCategoryHandler = async (req: Request, res: Response) => {
 export const getCategoryHandler = async (req: Request<ReadCategoryInput["params"]>, res: Response) => {
   try {
     const categoryId = req.params.categoryId;
+    try {
+      const categoryData = await redisClient.get(`category_${categoryId}`);
+      if (categoryData != null) return res.send(JSON.parse(categoryData));
+    } catch (error) {
+      logger.error(error);
+    }
     const category = await findCategory({ _id: categoryId });
     if (!category) {
       return res.sendStatus(404);
     }
+    await redisClient.setEx(`category_${categoryId}`, DEFAULT_EXPIRATION, JSON.stringify(category));
     return res.send(category);
   } catch (e: any) {
     logger.error(e);
@@ -49,7 +66,15 @@ export const getCategoryHandler = async (req: Request<ReadCategoryInput["params"
 
 export const getActiveCategoriesHandler = async (_req: Request<ReadCategoryInput["params"]>, res: Response) => {
   try {
+    try {
+      const categoryData = await redisClient.get("activeCategories");
+      if (categoryData != null) return res.send(JSON.parse(categoryData));
+    } catch (error) {
+      logger.error(error);
+    }
+
     const categories = await findCategories({ active: true });
+    await redisClient.setEx("activeCategories", DEFAULT_EXPIRATION, JSON.stringify(categories));
     return res.send(categories);
   } catch (e: any) {
     logger.error(e);
@@ -59,7 +84,14 @@ export const getActiveCategoriesHandler = async (_req: Request<ReadCategoryInput
 
 export const getDeActiveCategoriesHandler = async (_req: Request<ReadCategoryInput["params"]>, res: Response) => {
   try {
+    try {
+      const categoryData = await redisClient.get("deActiveCategories");
+      if (categoryData != null) return res.send(JSON.parse(categoryData));
+    } catch (error) {
+      logger.error(error);
+    }
     const categories = await findCategories({ active: false });
+    await redisClient.setEx("deActiveCategories", DEFAULT_EXPIRATION, JSON.stringify(categories));
     return res.send(categories);
   } catch (e: any) {
     logger.error(e);
@@ -80,6 +112,8 @@ export const changeActiveStatusHandler = async (req: Request, res: Response) => 
 
     await updateCategory({ _id: categoryId }, { active });
 
+    await redisClient.del([`category_${categoryId}`, "activeCategories", "deActiveCategories"]);
+
     return res.send(`Category ${active ? "activated" : "deactivated"} successfully!`);
   } catch (e: any) {
     logger.error(e);
@@ -97,6 +131,8 @@ export const deleteCategoryHandler = async (req: Request<DeleteCategoryInput["pa
     }
 
     await deleteCategory({ _id: category._id });
+
+    await redisClient.del([`category_${categoryId}`, "activeCategories", "deActiveCategories"]);
 
     return res.sendStatus(200);
   } catch (e: any) {
